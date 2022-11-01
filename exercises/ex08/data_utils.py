5a1b1c8a19eee961c0d209acf3927a7187c32d6d
Sanjana Nalla <sanjananalla19@gmail.com>
2022-10-31 22:32:22 -0400

    lesoson sup fasdfjhas d

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -4,7 +4,6 @@ __author__ = "730573834"
 
 # Define your functions below
 from csv import DictReader
-from turtle import fd
 
 
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
@@ -27,7 +26,7 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
     return result
 
 
-def column_values(table:list[dict[str,str]], column: str) -> list[str]:
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
     """Produce a list[str] of all values in a single column."""
     result: list[str] = []
 
@@ -48,47 +47,79 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     return result
 
 
-def head(inputs:dict[str, list[str]], N:int) -> dict[str, list[str]]:
-    """This function takes in a integer and dictionary and returns a new dictionary with ____ ."""
+def head(inputs: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """This function takes in a integer and dictionary and returns a new dictionary that stored all the columns(keys) with the first N items(values)."""
     output: dict[str, list[str]] = {}
 
+    # initialize a counter var
     i: int = 0
+    # access keys of inputs dict
     for column in inputs:
-        values:list[str] = []
+        # create new list
+        values: list[str] = []
         i = 0
+        # while the counter is less than the given N and less than the length of the values in the each key's value
         while ((i < N) and (i < len(inputs[column]))):
+            # add the the items in values list of keys to a new list
             values.append(inputs[column][i])
+            # add 1 to the counter variable
             i += 1
+        # add new key-value pairs to new dict
         output[column] = values
-
+    # return new dict
     return output
     
 
 def select(selection: dict[str, list[str]], selected: list[str]) -> dict[str, list[str]]:
-    """This function __."""
+    """This function takes in a dictionary and list to return a dictionary with only the columns specified in the given list(as keys) and the values it is paired to."""
+    # create a new dict
     output: dict[str, list[str]] = {}
 
+    # iterate through the given list's items which serve as the keys in the dict
     for column in selected:
+        # iterate through the associated values of the dictionary
         for row in selection[column]:
+            # add the key-value pair to the output dict
             output[column] = selection[column]
-
+    # return new dict
     return output
 
 
 def concat(dict_a: dict[str, list[str]], dict_b: dict[str, list[str]]) -> dict[str, list[str]]:
-    """This function ___."""
+    """This function takes in two dictionaries and returns a dictionary that combined them but for duplicate keys, the values of the two are just added together; otherwise, the key-value pairs were added to the new dictionary as is."""
+    # create a new dict
     output: dict[str, list[str]] = {}
 
+    # iterate through one dict's keys
     for column in dict_a:
+        # add the key-value pairs to the output dict
         output[column] = dict_a[column]
+    # iterate through the second dict's keys
     for column in dict_b:
-        output[column] = dict_b[column]
-
+        # if the key already in output dict, then add the key's values to the existing value
+        if column in output:
+            output[column] += dict_b[column]
+        # add the key-value pairs to the output dict
+        else:
+            output[column] = dict_b[column]
+    # return new dict
     return output
 
 
-def count(inputs:list[str]) -> dict[str, int]:
-    """This function ___."""
+def count(inputs: list[str]) -> dict[str, int]:
+    """This function takes in a list and returns a dictionary with the strings in the list as the keys and the values as the number times the string appeared in the list."""
+    # create a new dict
     output: dict[str, int] = {}
 
+    # iterate through items of the list
+    for item in inputs:
+        # if item is already in the output, then go into the then block 
+        if item in output:
+            # add one to the value associated with the key 
+            output[item] += 1
+        # if item is not already in the output, then go into this then block
+        else:
+            # assign the value associated with the key 1
+            output[item] = 1
+    # return new dict
     return output
---
+++ lessons/classes_objects.py
@@ -0,0 +1,37 @@
+"""Example of a class and object instantiation."""
+
+
+class Pizza: 
+    """Models the idea of a Pizza."""
+
+    # Attribute Definitions
+    size: str
+    toppings: int
+    extra_cheese: bool
+
+
+    def price(self) -> float:
+        """Calculate the price of a Pizza."""
+        total: float = 0.0
+        if self.size == "large":
+            total += 10.0
+        else:
+            total += 8.0
+        
+        total += self.toppings * 0.75
+
+        if self.extra_cheese:
+            total += 1.0
+
+        return total
+
+
+a_pizza: Pizza = Pizza()
+a_pizza.size = "large"
+a_pizza.toppings = 3
+a_pizza.extra_cheese = False
+
+print(Pizza)
+print(a_pizza)
+print(a_pizza.size)
+print(f"Price: ${price(a_pizza)}")
