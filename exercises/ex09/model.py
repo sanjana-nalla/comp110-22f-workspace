635f02fdfcbef2b80d3a75a83fadbbd516029109
Sanjana Nalla <sanjananalla19@gmail.com>
2022-11-06 18:14:01 -0500

    fsdhlkajd

--- exercises/ex09/model.py
+++ exercises/ex09/model.py
@@ -6,7 +6,7 @@ from exercises.ex09 import constants
 from math import sin, cos, pi
 
 
-__author__ = ""  # TODO
+__author__ = "730573834"
 
 
 class Point:
@@ -30,7 +30,28 @@ class Cell:
     """An individual subject in the simulation."""
     location: Point
     direction: Point
-    sickness: int = 0
+    sickness: int = constants.VULNERABLE
+
+    def contract_disease(self) -> None:
+        self.sickness = constants.INFECTED
+    
+    def is_vulnerable(self) -> bool:
+        if (self.sickness == 0):
+            return True
+        else:
+            return False
+
+    def is_infected(self) -> bool:
+        if (self.sickness == 1):
+            return True
+        else:
+            return False
+
+    def color(self) -> str:
+        if (is_vulnerable() == True):
+            return "gray"
+        if (is_infected() == True):
+            return "red"
 
     def __init__(self, location: Point, direction: Point):
         """Construct a cell with its location and direction."""
@@ -41,6 +62,10 @@ class Cell:
     # Its purpose is to reassign the object's location attribute
     # the result of adding the self object's location with its
     # direction. Hint: Look at the add method.
+
+    def tick(self) -> None:
+        self.location = self.location.add(self.direction)
+
         
     def color(self) -> str:
         """Return the color representation of a cell."""
@@ -56,24 +81,40 @@ class Model:
     def __init__(self, cells: int, speed: float):
         """Initialize the cells with random locations and directions."""
         self.population = []
+        for _ in range(0, cells):
+            start_location: Point = self.random_location()
+            start_direction: Point = self.random_direction(speed)
+            cell: Cell = Cell(start_location, start_direction)
+            self.population.append(cell)
+        
     
     def tick(self) -> None:
         """Update the state of the simulation by one time step."""
         self.time += 1
+        for cell in self.population:
+            cell.tick()
+            self.enforce_bounds(cell)
 
     def random_location(self) -> Point:
         """Generate a random location."""
+        start_x: float = random() * constants.BOUNDS_WIDTH - constants.MAX_X
+        start_y: float = random() * constants.BOUNDS_HEIGHT - constants.MAX_Y
         # TODO
-        return Point(0.0, 0.0)
+        return Point(start_x, start_y)
 
     def random_direction(self, speed: float) -> Point:
         """Generate a 'point' used as a directional vector."""
+        random_angle: float = 2.0 * pi * random()
+        direction_x: float = cos(random_angle) * speed
+        direction_y: float = sin(random_angle) * speed
         # TODO
-        return Point(0.0, 0.0)
+        return Point(direction_x, direction_y)
 
     def enforce_bounds(self, cell: Cell) -> None:
         """Cause a cell to 'bounce' if it goes out of bounds."""
-        ...
+        if (cell.location.x > constants.MAX_X):
+            cell.location.x = constants.MAX_X
+            cell.direction.x *= -1.0
 
     def is_complete(self) -> bool:
         """Method to indicate when the simulation is complete."""
