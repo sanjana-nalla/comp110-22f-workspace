e9d48bec48d728057dcd62eef7b40008b34a66b5
Sanjana Nalla <sanjananalla19@gmail.com>
2022-11-06 16:48:28 -0500

    preex090

--- lessons/classes_objects.py
+++ lessons/classes_objects.py
@@ -10,7 +10,14 @@ class Pizza:
     extra_cheese: bool
 
 
-    def price(self) -> float:
+def __init__(self, size: str, toppings: int, extra_cheese: bool):
+    """Constructor definition for intialization of attributes."""
+    self.size = size
+    self.toppings = toppings
+    self.extra_cheese = extra_cheese
+
+
+    def price(self, tax: float) -> float:
         """Calculate the price of a Pizza."""
         total: float = 0.0
         if self.size == "large":
@@ -22,6 +29,8 @@ class Pizza:
 
         if self.extra_cheese:
             total += 1.0
+        
+        total *= tax
 
         return total
 
@@ -34,4 +43,11 @@ a_pizza.extra_cheese = False
 print(Pizza)
 print(a_pizza)
 print(a_pizza.size)
-print(f"Price: ${price(a_pizza)}")
+# print(f"Price: ${price(a_pizza)}")
+print(f"Price: ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+print(another_pizza.size)
+print(f"Price: ${another_pizza.price(1.05)}")
+
+
